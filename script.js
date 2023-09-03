3b800fcbb95cdde6b10d26e3eb328536abd8baee
Anshuman Bahekar <78291316+anshumanbahekar@users.noreply.github.com>
2023-09-03 17:38:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// W00t! How the hell did you do that without Javascript? 
+//
+// This pen is part of a series of CSS-only SVG animations intended to progressively explain how SVG images can be animated in a cool way without using any Javascript and, of course, without ending with a hell of a mess in the Sass file/s.
+// Feel free to toy around. 
+// That's precisely why I created these pens for. :). 
+//
+// P.S.: Please note that this pen may only work on Webkit/Blink browsers due to a bug with transform-origin on Firefox.
+// CREDITS:
+// <a rel="nofollow" target="_blank" href="https://www.vecteezy.com/">Street sign and Walk cycle designed by www.Vecteezy.com</a>
+// <a href="http://www.freepik.com/free-photos-vectors/background">Background vector created by Frimufilms - Freepik.com</a>
+// <a href="http://www.freepik.com/free-photos-vectors/food">Food vector created by Freepik</a>
